a091de8e2a7ef303fd6f83baa6f3094463f1d476
David Burke <david.burke@aakonsult.com>
2024-05-24 10:34:56 +1000

    Updated to README for v1.0.0. Added about and git link to configure page

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,5 @@
 module.exports = function(grunt) {
-    const version = '0.3.1';
+    const version = '1.0.0';
     // Project configuration.
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
